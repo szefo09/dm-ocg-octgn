3749131efd78b34691a5fd3811f7f302bcfb5183
szefo09 <37833135+szefo09@users.noreply.github.com>
2024-11-05 11:21:12 +0100

    refactor silentSkill

--- bb784fc6-fe21-4603-90d7-82c049908a74/scripts/actions.py
+++ bb784fc6-fe21-4603-90d7-82c049908a74/scripts/actions.py
@@ -1810,7 +1810,9 @@ def untapAll(group=table, isNewTurn=False, x=0, y=0):
 			continue
 		# Untap Creatures
 		if card.orientation == Rot90:
-			if isNewTurn and isCreature(card) and not isBait(card) and cardScripts.get(card.name, {}).get('silentSkill', []):
+			if not isNewTurn:
+				card.orientation = Rot0
+			elif isCreature(card) and not isBait(card) and cardScripts.get(card.name, {}).get('silentSkill', []):
 				choice = askYN("Activate Silent Skill for {}?\n\n{}".format(card.Name, card.Rules), ["Yes", "No"])
 				if choice == 1:
 					notify('{} uses Silent Skill of {}'.format(me, card))
@@ -1827,8 +1829,6 @@ def untapAll(group=table, isNewTurn=False, x=0, y=0):
 					evaluateWaitingFunctions()
 				else:
 					card.orientation = Rot0
-			else: 
-				card.orientation = Rot0
 
 		# Untap Mana
 		if card.orientation == Rot270:
